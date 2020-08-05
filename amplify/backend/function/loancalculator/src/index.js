517c5e417561c3d3a493821f458108d50464fab5
mzarnawski <mzarnawski@gmail.com>
2020-08-06 00:24:01 +0200

    Open CORS

--- amplify/backend/function/loancalculator/src/index.js
+++ amplify/backend/function/loancalculator/src/index.js
@@ -1,10 +1,11 @@
-
-
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
-};
+  // TODO implement
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify('Hello from Lambda!'),
+  }
+  return response
+}
