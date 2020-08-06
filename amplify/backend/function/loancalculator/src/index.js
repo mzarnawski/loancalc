174906c3e0a2d5589f153e72995e95f9f872dc3d
mzarnawski <mzarnawski@gmail.com>
2020-08-06 13:17:24 +0200

    Function body

--- amplify/backend/function/loancalculator/src/index.js
+++ amplify/backend/function/loancalculator/src/index.js
@@ -1,6 +1,46 @@
 exports.handler = async (event) => {
-  // TODO implement
-  let loan_plan = {}
+  let loan_plan = []
+
+  const total_amount = event.body.amount
+  const duration = event.body.duration
+  const interest = event.body.interest
+
+  console.log(duration)
+
+  const monthly_interest = interest / 100 / 12
+  const monthly_multiplier = 1 + monthly_interest
+
+  const monthly_rate =
+    total_amount *
+    ((monthly_interest * Math.pow(monthly_multiplier, duration)) /
+      (Math.pow(monthly_multiplier, duration) - 1))
+  const monthly_money = Math.round(monthly_rate * 100) / 100
+
+  let principal_left = total_amount
+
+  for (let i = 1; i <= duration; i++) {
+    let rate = {}
+    rate.month = i
+
+    if (i === duration) {
+      rate.total = Math.round(principal_left * monthly_multiplier * 100) / 100
+      rate.interest = Math.round(principal_left * monthly_interest * 100) / 100
+      rate.principal = Math.round((rate.total - rate.interest) * 100) / 100
+      principal_left = 0
+    } else {
+      rate.total = monthly_money
+      rate.interest = Math.round(principal_left * monthly_interest * 100) / 100
+      rate.principal = Math.round((rate.total - rate.interest) * 100) / 100
+      principal_left =
+        Math.round(
+          (principal_left * monthly_multiplier - monthly_money) * 100
+        ) / 100
+    }
+
+    rate.principal_left = principal_left
+
+    loan_plan.push(rate)
+  }
 
   const response = {
     statusCode: 200,
